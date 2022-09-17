33adfb4379c6b5f839be661084b96ef65494ebcb
Renato <renatomarquesteles@gmail.com>
2022-09-17 14:02:36 -0300

    backend: adds routes, Game and Ad models and migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,33 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/ads', (req, res) => {
+// List all games
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+// Create a new Ad
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+// List all game ads
+app.get('/games/:id/ads', (req, res) => {
+  const gameId = req.params.id;
+
   return res.json([
-    { id: 1, name: 'First Ad'},
-    { id: 2, name: 'Second Ad'},
-    { id: 3, name: 'Third Ad'},
-  ])
-})
+    { id: 1, name: 'First Ad' },
+    { id: 2, name: 'Second Ad' },
+    { id: 3, name: 'Third Ad' },
+  ]);
+});
 
-app.listen(3333)
+// Gets discord from an ad
+app.get('/ads/:id/discord', (req, res) => {
+  const adId = req.params.id;
+
+  return res.json([]);
+});
+
+app.listen(3333);
