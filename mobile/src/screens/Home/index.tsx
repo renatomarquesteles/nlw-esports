c4ebd6365f37a020c19b59aa1b0d846789d1697e
Renato <renatomarquesteles@gmail.com>
2022-09-17 11:10:00 -0300

    mobile: adds Home screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
+import { FlatList, Image, View } from 'react-native';
+
+import logo from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+
+      <Heading
+        title="Find your duo!"
+        subtitle="Select the game that you want to play..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        contentContainerStyle={styles.contentList}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
 }
